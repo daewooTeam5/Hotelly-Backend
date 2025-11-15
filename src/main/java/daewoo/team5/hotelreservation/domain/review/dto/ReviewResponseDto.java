7263c6472cb7bcaa8dd3a6c24797c0b82259b36f
devaspirant0510 <seungho020510@gmail.com>
2025-11-15 23:21:27 +0900

    refactor: 리뷰 관련 패키지 경로 변경

--- src/main/java/daewoo/team5/hotelreservation/domain/review/dto/ReviewResponseDto.java
+++ src/main/java/daewoo/team5/hotelreservation/domain/review/dto/ReviewResponseDto.java
@@ -1,4 +1,4 @@
-package daewoo.team5.hotelreservation.domain.place.review.dto;
+package daewoo.team5.hotelreservation.domain.review.dto;
 
 import daewoo.team5.hotelreservation.domain.users.entity.UsersEntity;
 import lombok.AllArgsConstructor;
